30baf738a1b624ec666aa5c8636a811e4ad62b04
MohanManirul <fiforeg@gmail.com>
2025-01-06 21:21:19 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+// src/App.jsx
+import React from 'react';
+import './index.css';
+
+function App() {
+  return (
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl font-bold text-red-900">Hello, Tailwind CSS!</h1>
+    </div>
+  );
+}
+
+export default App;
+
+
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/MohanManirul/figmaPractice.git
+git push -u origin main
